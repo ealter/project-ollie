9cc78d9edcee2c781dbdc7119349263c45932d69
Steven Gregory <sdg227@gmail.com>
2012-06-04 11:17:39 +1300

    improved terrain

--- ProjectOllie/Terrain.h
+++ ProjectOllie/Terrain.h
@@ -8,19 +8,32 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
+#import "gpc.h"
+#import "Box2D.h"
 
-typedef enum {
-    inside,
-    outside	 
-} LandType;
-
+struct landBlock
+{
+    gpc_polygon poly;           //Adding/subtracting (real model)
+    b2Body      *body;          //Physics (derived)
+    ccVertex2F  *triangleVerts; //Drawing (derived)
+    NSArray     *texCoords;     //Drawing (derived)
+};
 
 @interface Terrain : CCNode {
-    NSMutableArray *viewPolygons;
-    
+    NSMutableArray *landBlocks;
+    CCTexture2D *texture;           
 }
 
-//+ (id) generateRandomWith
+//Building land
+- (void) addPolygon:(gpc_polygon)p;
 
+//Removing land
+- (void) removeCircleWithRadius:(float)r x:(float)x y:(float)y;
+- (void) removePolygon:(gpc_polygon)p;
+
++ (id) generateRandomIsland;
++ (id) generateRandomTwoIsland;
++ (id) generateRandomBlobs;
++ (id) generateRandomCavern;
 
 @end
