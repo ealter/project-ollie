9a30a46db217a48c4d7cbf75fed0cb82c6ac07c9
Tucker Stone <tuckstone@gmail.com>
2012-06-03 11:50:01 -1000

    Draw code beginning

--- ProjectOllie/DrawEnvironment.mm
+++ ProjectOllie/DrawEnvironment.mm
@@ -10,7 +10,30 @@
 #import "AppDelegate.h"
 #import "PhysicsSprite.h"
 @implementation DrawEnvironment
+@synthesize gpc_polys, newpoly;
 
+-(id) init
+{
+	if(self=[super init]) {
+        
+	}
+	return self;
+}
 
+-(void)ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    newpoly = [[polywrapper alloc] init];
+    
+}
+
+-(void)ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [gpc_polys addObject:newpoly];
+}
 
 @end
---
+++ polywrapper.mm
@@ -0,0 +1,13 @@
+//
+//  polywrapper.m
+//  ProjectOllie
+//
+//  Created by Lion User on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "polywrapper.h"
+
+@implementation polywrapper
+@synthesize gpc_polygon;
+@end
