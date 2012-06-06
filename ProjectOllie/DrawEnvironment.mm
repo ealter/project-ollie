b23b686c0499ef8708ae0811d97b932755086f02
Steven Gregory <sdg227@gmail.com>
2012-06-07 10:58:48 +1300

    land drawing

--- ProjectOllie/DrawEnvironment.mm
+++ ProjectOllie/DrawEnvironment.mm
@@ -40,50 +40,53 @@
         
         //Code to make small circle
         smallcircle = new gpc_polygon;
-        smallcircle->hole=0;
+        smallcircle->hole=new int(0);
         smallcircle->contour = new vertex_list;
-        smallcircle->contour->vertex = new ccVertex2F[numpoints];
-        smallcircle->num_contours +=1;
+        smallcircle->contour[0].vertex = new ccVertex2F[numpoints];
+        smallcircle->contour[0].num_vertices = 0;
+        smallcircle->num_contours =1;
         float anglecount = 0;
         for (int i =0; i<numpoints; i++) {
             ccVertex2F newvertex;
             newvertex.x = (sinf(anglecount*M_PI/180)*smallradius);
             newvertex.y = (cosf(anglecount*M_PI/180)*smallradius);
-            smallcircle->contour->num_vertices += 1;
-            smallcircle->contour->vertex[i] = newvertex;
-            anglecount += (360/numpoints);
+            smallcircle->contour[0].num_vertices += 1;
+            smallcircle->contour[0].vertex[i] = newvertex;
+            anglecount += (360.0f/numpoints);
         }
         
         //Medium circle
         mediumcircle = new gpc_polygon;
-        mediumcircle->hole=0;
+        mediumcircle->hole=new int(0);
         mediumcircle->contour = new vertex_list;
-        mediumcircle->contour->vertex = new ccVertex2F[numpoints];
-        mediumcircle->num_contours +=1;
+        mediumcircle->contour[0].vertex = new ccVertex2F[numpoints];
+        mediumcircle->contour[0].num_vertices = 0;
+        mediumcircle->num_contours =1;
         anglecount = 0;
         for (int i =0; i<numpoints; i++) {
             ccVertex2F newvertex;
             newvertex.x = (sinf(anglecount*M_PI/180)*mediumradius);
             newvertex.y = (cosf(anglecount*M_PI/180)*mediumradius);
-            mediumcircle->contour->num_vertices += 1;
-            mediumcircle->contour->vertex[i] = newvertex;
-            anglecount += (360/numpoints);
+            mediumcircle->contour[0].num_vertices += 1;
+            mediumcircle->contour[0].vertex[i] = newvertex;
+            anglecount += (360.0f/numpoints);
         }
         
         //Large circle
         largecircle = new gpc_polygon;
-        largecircle->hole=0;
+        largecircle->hole=new int(0);
         largecircle->contour = new vertex_list;
-        largecircle->contour->vertex = new ccVertex2F[numpoints];
-        largecircle->num_contours +=1;
+        largecircle->num_contours =1;
+        largecircle->contour[0].vertex = new ccVertex2F[numpoints];
+        largecircle->contour[0].num_vertices = 0;
         anglecount = 0;
         for (int i =0; i<numpoints; i++) {
             ccVertex2F newvertex;
             newvertex.x = (sinf(anglecount*M_PI/180)*largeradius);
             newvertex.y = (cosf(anglecount*M_PI/180)*largeradius);
-            largecircle->contour->num_vertices += 1;
-            largecircle->contour->vertex[i] = newvertex;
-            anglecount += (360/numpoints);
+            largecircle->contour[0].num_vertices += 1;
+            largecircle->contour[0].vertex[i] = newvertex;
+            anglecount += (360.0f/numpoints);
         }
         brush = smallcircle;
         brushradius = smallradius;
@@ -96,11 +99,12 @@
 {
     UITouch *touch = [[event allTouches] anyObject];
     CGPoint location = [touch locationInView:[touch view]];
+    location.y *= -1;
     prevpoint = location;
     
     gpc_polygon *newcircle = gpc_offset_clone(brush, location.x, location.y);
     [terrain addPolygon:newcircle];
-    gpc_free_polygon(newcircle);
+    //gpc_free_polygon(newcircle);
     delete newcircle;
 }
 
@@ -108,8 +112,9 @@
 {
     UITouch *touch = [[event allTouches] anyObject];
     CGPoint location = [touch locationInView:[touch view]];
+    location.y *= -1;
     //makes sure the circles are far enough away to merit new circle
-    if (sqrt((location.x-prevpoint.x)*(location.x-prevpoint.x)+(location.y-prevpoint.y)*(location.y-prevpoint.y))>4)
+    if (sqrt((location.x-prevpoint.x)*(location.x-prevpoint.x)+(location.y-prevpoint.y)*(location.y-prevpoint.y))>10)
     {
         gpc_polygon *newcircle = gpc_offset_clone(brush, location.x, location.y);
         [terrain addPolygon:newcircle];
@@ -129,6 +134,7 @@
 {
     UITouch *touch = [[event allTouches] anyObject];
     CGPoint location = [touch locationInView:[touch view]];
+    location.y *= -1;
     
     gpc_polygon *newcircle = gpc_offset_clone(brush, location.x, location.y);
     [terrain addPolygon:newcircle];
@@ -143,18 +149,19 @@
 
 
 //MAKE SURE YOU FREE THE RECTANGLE AFTER YOU MAKE IT
--(gpc_polygon *)rectangleMakeWithPoint:(CGPoint)pointa andPoint:(CGPoint)pointb withWidth:(int)width
+-(gpc_polygon *)rectangleMakeWithPoint:(CGPoint)pointa andPoint:(CGPoint)pointb withWidth:(float)width
 {
     gpc_polygon *rectangle = new gpc_polygon;
-    rectangle->hole=0;
+    rectangle->hole=new int(0);
     rectangle->num_contours = 1;
     
     //Make unit vector between the two points
     CGPoint unitvector;
     unitvector.x = (pointa.x - pointb.x);
     unitvector.y = (pointa.y - pointb.y);
-    unitvector.x = unitvector.x/sqrt((unitvector.x*unitvector.x) + (unitvector.y*unitvector.y));
-    unitvector.y = unitvector.y/sqrt((unitvector.x*unitvector.x) + (unitvector.y*unitvector.y));
+    float len = sqrt((unitvector.x*unitvector.x) + (unitvector.y*unitvector.y));
+    unitvector.x = unitvector.x/len;
+    unitvector.y = unitvector.y/len;
     
     //Rotate vector by 90 degrees, multiply by desired width
     float holdy = unitvector.y;
@@ -173,10 +180,10 @@
     corner1.y = pointa.y+unitvector.y;
     corner2.x = pointa.x-unitvector.x;
     corner2.y = pointa.y-unitvector.y;
-    corner4.x = pointb.x+unitvector.x;
-    corner4.y = pointb.y+unitvector.y;
     corner3.x = pointb.x-unitvector.x;
     corner3.y = pointb.y-unitvector.y;
+    corner4.x = pointb.x+unitvector.x;
+    corner4.y = pointb.y+unitvector.y;
     
     rectangle->contour = new vertex_list;
     rectangle->contour->num_vertices=4;
@@ -184,7 +191,7 @@
     rectangle->contour->vertex[0]=corner1;
     rectangle->contour->vertex[1]=corner2;
     rectangle->contour->vertex[2]=corner3;
-    rectangle->contour->vertex[4]=corner4;
+    rectangle->contour->vertex[3]=corner4;
     
     return rectangle;
 }
