717beb127effd7c330c24916611b197f2820ed33
Tucker Stone <tuckstone@gmail.com>
2012-06-06 15:59:03 -1000

    Made Sandbox Scene

---
+++ ProjectOllie/SandboxScene.h
@@ -0,0 +1,16 @@
+//
+//  SandboxScene.h
+//  ProjectOllie
+//
+//  Created by Lion User on 6/6/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface SandboxScene : CCScene {
+    
+}
+
+@end
