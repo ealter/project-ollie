8c1b56547890d7337116c6fb6245ca5d89e7b03d
Tucker Stone <tuckstone@gmail.com>
2012-06-11 13:40:18 -1000

    Drawmenu interface, doesn't do anything yet

---
+++ ProjectOllie/DrawMenu.h
@@ -0,0 +1,17 @@
+//
+//  DrawMenu.h
+//  ProjectOllie
+//
+//  Created by Lion User on 6/11/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "gpc.h"
+@interface DrawMenu : CCLayer {
+    
+}
+
+
+@end
