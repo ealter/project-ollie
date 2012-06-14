065d436562f74e53b753a5ec36c11019f9964be0
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-14 13:44:24 -1000

    fixed compiler warning

--- ProjectOllie/SandboxScene.h
+++ ProjectOllie/SandboxScene.h
@@ -8,15 +8,11 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
-#import "ActionLayer.h";
+#import "ActionLayer.h"
 #import "CCScene.h"
 
+@interface SandboxScene : CCScene
 
-@interface SandboxScene : CCScene {
-    
-    
-    
-}
 @property (strong, nonatomic) ActionLayer* actionLayer;
 
 @end
