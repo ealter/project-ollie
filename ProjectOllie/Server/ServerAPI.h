2456f0309bbf68d4b871e1bba67d18e5ad1834b7
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-18 15:08:42 -1000

    Added capabilities to use a dev server

--- ProjectOllie/Server/ServerAPI.h
+++ ProjectOllie/Server/ServerAPI.h
@@ -13,6 +13,18 @@
 #define SERVER_AUTH_TOKEN_KEY @"auth_token"
 #define SERVER_ERROR_KEY @"error"
 
+#ifndef SERVER_IS_DEV
+#define SERVER_IS_DEV 0
+#endif
+
+#ifndef DOMAIN_NAME
+#if SERVER_IS_DEV
+#define DOMAIN_NAME @"http://dev.gorillawarefaregame.com"
+#else
+#define DOMAIN_NAME @"http://106.187.44.7"
+#endif /* SERVER_IS_DEV */
+#endif /* DOMAIN_NAME */
+
 /* Used to handle interactions with the server that would otherwise be pretty boilerplate. Subclasses should implement - (void)connectionDidFinishLoading:(NSURLConnection *)connection
  */
 @interface ServerAPI : NSObject <NSURLConnectionDataDelegate> {
