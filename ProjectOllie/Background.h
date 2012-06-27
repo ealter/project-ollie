d194ef544170ddfddb1ade3d92dc4a59058688e6
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-27 11:46:33 -1000

    Cleaned up some code in Background

--- ProjectOllie/Background.h
+++ ProjectOllie/Background.h
@@ -19,7 +19,7 @@
 @property (assign, nonatomic) float scrollSpeed; /* In pixels/second. Positive means left to right, negative means right to left. */
 @property (nonatomic, strong) NSArray *imageNames; //An array of NSStrings
 
-- (id)initWithSpeed:(int)speed images:(NSArray *)imageNames;
-- (void)scroll:(ccTime)dt;
+/* Note: It is fine to call the regular init method instead of this one. If you do that, make sure to set the imageNames at some point or else this class will not display anything. */
+- (id)initWithSpeed:(float)speed images:(NSArray *)imageNames;
 
 @end
