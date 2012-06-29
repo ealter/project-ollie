ac9f7c5cbdf80dea46a0199d63f96373a3e640e3
Eliot Alter <Eliot.Alter@tufts.edu>
2012-06-29 13:01:14 -1000

    Moved the ptm ratio into its own file

---
+++ ProjectOllie/GameConstants.m
@@ -0,0 +1,13 @@
+//
+//  GameConstants.m
+//  ProjectOllie
+//
+//  Created by Eliot Alter on 6/29/12.
+//  Copyright (c) 2012 hi ku LLC. All rights reserved.
+//
+
+#import "GameConstants.h"
+
+@implementation GameConstants
+
+@end
