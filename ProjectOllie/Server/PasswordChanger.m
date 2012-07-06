850189901c2f031f81eec8f20570a6af418d2dd7
Eliot Alter <Eliot.Alter@tufts.edu>
2012-07-06 12:55:51 -1000

    Added checks to make sure the program didn't crash when missing data for password changing

--- ProjectOllie/Server/PasswordChanger.m
+++ ProjectOllie/Server/PasswordChanger.m
@@ -21,7 +21,13 @@
         [self broadcastServerOperationFailedWithError:@"Unknown username"];
         return;
     }
-    NSDictionary *requestData = [[NSDictionary alloc]initWithObjects:[NSArray arrayWithObjects:self.auth.username, self.auth.authToken, oldPassword, newPassword, nil] forKeys:[NSArray arrayWithObjects:@"username", SERVER_AUTH_TOKEN_KEY, @"oldPassword", @"newPassword", nil]];
+    if(!self.auth.authToken) {
+        [self broadcastServerOperationFailedWithError:@"Missing authorization token. Please try logging in again"];
+        return;
+    }
+    NSMutableDictionary *requestData = [[NSMutableDictionary alloc]initWithObjects:[NSArray arrayWithObjects:self.auth.username, self.auth.authToken, newPassword, nil] forKeys:[NSArray arrayWithObjects:@"username", SERVER_AUTH_TOKEN_KEY, @"newPassword", nil]];
+    if(oldPassword)
+        [requestData setObject:oldPassword forKey:@"oldPassword"];
     [self makeServerRequestWithData:requestData url:[[self class] urlForPageName:@"changePassword"]];
 }
 
