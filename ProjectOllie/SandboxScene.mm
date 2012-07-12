757e401377504be36f00d3864be3b29870352306
Eliot Alter <Eliot.Alter@tufts.edu>
2012-07-12 13:16:18 -1000

    Made the project compatible with ios 4.0

--- ProjectOllie/SandboxScene.mm
+++ ProjectOllie/SandboxScene.mm
@@ -25,7 +25,7 @@
         
         /* Set up action layer (where action occurs */
         self.actionLayer = [ActionLayer node];
-        Background *blayer = [[Background node] initWithSpeed:30.0 images:[NSArray arrayWithObject:@"background_layer1_middleplants.png"]];
+        Background *blayer = [[Background node] initWithSpeed:30.0 images:[NSArray arrayWithObject:@"background.jpg"]];
         [self.actionLayer.camera.children addObject:blayer];
         
         CCNode* actionNode = [CCNode node];
