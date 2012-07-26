3f373ce5fb86b2e82b802b554be9490eedc74636
Tucker Stone <tuckstone@gmail.com>
2012-07-25 16:47:17 -1000

    adding gesture protocol and trajectory plotting

--- ProjectOllie/GWObjects/GWGestures.h
+++ ProjectOllie/GWObjects/GWGestures.h
@@ -9,8 +9,29 @@
 #import <Foundation/Foundation.h>
 #import "CCLayer.h"
 
+@protocol GestureChild <NSObject>
+@optional
+-(void)handleSwipeRightWithAngle:(float) angle andLength:(float) length andVelocity:(float) velocity;
+
+-(void)handleSwipeLeftWithAngle:(float) angle andLength:(float) length andVelocity:(float) velocity;
+
+-(void)handleSwipeUpWithAngle:(float) angle andLength:(float) length andVelocity:(float) velocity;
+
+-(void)handleSwipeDownWithAngle:(float) angle andLength:(float) length andVelocity:(float) velocity;
+
+-(void)handlePanWithStart:(CGPoint) startPoint andCurrent:(CGPoint) currPoint andTime:(float) time;
+
+-(void)handlePanFinishedWithStart:(CGPoint) startPoint andEnd:(CGPoint) endPoint andTime:(float) time;
+
+-(void)handleTap:(CGPoint) tapPoint;
+
+@end
+
 @interface GWGestures : CCLayer {
     
 }
+
+@property (strong, nonatomic) NSMutableArray* children; // Nodes that need gesture activation
+
 
 @end
