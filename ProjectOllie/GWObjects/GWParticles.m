3d8e938eb2b0c4e8e52fc1d6595ff4a55c51bc44
Tucker Stone <tuckstone@gmail.com>
2012-07-30 20:14:16 -1000

    Explosions push ragdolls, delete land, also changing company name on files made by LION USZER:

--- ProjectOllie/GWObjects/GWParticles.m
+++ ProjectOllie/GWObjects/GWParticles.m
@@ -3,7 +3,7 @@
 //  ProjectOllie
 //
 //  Created by Lion User on 6/30/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 hi ku llc. All rights reserved.
 //
 
 #import "GWParticles.h"
@@ -358,21 +358,21 @@ const static ccColor4F kColor4fZero = {0,0,0,0};
 {
     if( (self=[super initWithTotalParticles:p]) ) {
         // duration
-        duration = 0.15;
+        duration = 0.01;
         
         // Gravity Mode
         self.emitterMode = kCCParticleModeGravity;
         
         // Gravity Mode: gravity
-        self.gravity = ccp(0, 0);
+        self.gravity = ccp(0, -500);
         
         // Gravity Mode:  radial
         self.radialAccel = 0;
         self.radialAccelVar = 0;
         
         //  Gravity Mode: speed of particles
-        self.speed = 112;
-        self.speedVar = 380;
+        self.speed = 0;
+        self.speedVar = 500;
         
         // emitter position
         CGSize winSize = [[CCDirector sharedDirector] winSize];
@@ -384,32 +384,32 @@ const static ccColor4F kColor4fZero = {0,0,0,0};
         angleVar = 360;
         
         // life of particles
-        life = 0.4f;
-        lifeVar = 0.3;
+        life = 0.f;
+        lifeVar = 1.;
         
         // emits per frame
         emissionRate = totalParticles/life;
         
         // color of particles
-        startColor    = ccc4f(1., 0.09, 0., 0.);
-        startColorVar = kColor4fZero;
-        endColor      = ccc4f(1.,  1.,  0.0,  0.);
+        startColor    = ccc4f(1., 0.0, 0., 1.);
+        startColorVar = ccc4f(1., .22, 0., 0.5);
+        endColor      = ccc4f(0.,  0.,  0.0,  0.31);
         endColorVar   = kColor4fZero;
         
         // size, in pixels
-        startSize = 21.0f;
-        startSizeVar = 34.0f;
-        endSize = 0.f;
+        startSize = 20.0f;
+        startSizeVar = 6.0f;
+        endSize = 2.f;
         endSizeVar = 0.0f;
         
         //Change the GL blend function
-        [self setBlendFunc:(ccBlendFunc) { GL_ONE, GL_ONE_MINUS_SRC_ALPHA }];        
+        [self setBlendFunc:(ccBlendFunc) { GL_SRC_ALPHA, GL_ONE }];        
         
         //Get the particle texture0
         self.texture = [[CCTextureCache sharedTextureCache] addImage: @"fire.png"];
         
         // additive
-        self.blendAdditive = NO;
+        self.blendAdditive = YES;
     }
     
     return self;
