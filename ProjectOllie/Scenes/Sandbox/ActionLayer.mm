731e5bb767e9f80cf0fb3be6929f40e23342d7b2
Sam Zeckendorf <sam.zeckendorf@gmail.com>
2012-07-31 21:36:50 -1000

    small refactoring

--- ProjectOllie/Scenes/Sandbox/ActionLayer.mm
+++ ProjectOllie/Scenes/Sandbox/ActionLayer.mm
@@ -183,7 +183,7 @@ enum {
     fixtureDef.filter.maskBits = MASK_TERRAIN;
     groundBody->CreateFixture(&fixtureDef);
     
-    _character = [[GWCharacter alloc]initWithIdentifier:@"character" spriteIndices:[NSArray array] box2DWorld:world];
+    _character = [[GWCharacter alloc]initWithIdentifier:@"construction" spriteIndices:[NSArray array] box2DWorld:world];
     
     GaussRifle* gaussWeapon = [[GaussRifle alloc] initGunWithPosition:CGPointMake(1, 1) ammo:10 box2DWorld:world gameWorld:self];
     [self addChild:gaussWeapon];
