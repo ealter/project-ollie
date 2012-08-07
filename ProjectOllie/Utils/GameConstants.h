c85c7a0a135cbfb9ed0c41008296d6c6a176f5e9
Eliot Alter <Eliot.Alter@tufts.edu>
2012-08-07 11:56:40 -1000

    Making stuff line up

--- ProjectOllie/Utils/GameConstants.h
+++ ProjectOllie/Utils/GameConstants.h
@@ -1,23 +1,23 @@
 #ifndef ProjectOllie_GameContants_h
 #define ProjectOllie_GameContants_h
 
-#define PTM_RATIO 256    //Pixels to meters at a zoom level of 1
+#define PTM_RATIO 256 //Pixels to meters at a zoom level of 1
 
 #define FPS 60
 
 //Width and height of the world in meters
 #define WORLD_WIDTH 10
 #define WORLD_HEIGHT 6
-#define WORLD_WIDTH_PX (WORLD_WIDTH*PTM_RATIO)
-#define WORLD_HEIGHT_PX (WORLD_HEIGHT*PTM_RATIO)
+#define WORLD_WIDTH_PX  (WORLD_WIDTH  * PTM_RATIO)
+#define WORLD_HEIGHT_PX (WORLD_HEIGHT * PTM_RATIO)
 
 //For things that must go to the edges of all possible screens
-#define MIN_VIEWABLE_X (-WORLD_WIDTH*PTM_RATIO/2)
-#define MAX_VIEWABLE_X (WORLD_WIDTH*PTM_RATIO*3/2)
-#define MIN_VIEWABLE_Y (-WORLD_HEIGHT*PTM_RATIO/2)
-#define MAX_VIEWABLE_Y (WORLD_HEIGHT*PTM_RATIO*3/2)
+#define MIN_VIEWABLE_X (-WORLD_WIDTH  *PTM_RATIO/2)
+#define MAX_VIEWABLE_X ( WORLD_WIDTH  *PTM_RATIO*3/2)
+#define MIN_VIEWABLE_Y (-WORLD_HEIGHT *PTM_RATIO/2)
+#define MAX_VIEWABLE_Y (WORLD_HEIGHT  *PTM_RATIO*3/2)
 
-#define MAX_WIDTH_PX (MAX_VIEWABLE_X - MIN_VIEWABLE_X)
+#define MAX_WIDTH_PX  (MAX_VIEWABLE_X - MIN_VIEWABLE_X)
 #define MAX_HEIGHT_PX (MAX_VIEWABLE_Y - MIN_VIEWABLE_Y)
 
 #define CATEGORY_BONES       0x0001 // 0000000000000001 in binary
@@ -26,11 +26,11 @@
 #define CATEGORY_INTERACTOR  0x0008 // 0000000000001000 in binary
 #define CATEGORY_PELLETS     0x0016 // 0000000000010000 in binary
 
-#define MASK_BONES  (CATEGORY_TERRAIN | CATEGORY_PROJECTILES)                       // Collides with terrain and projectiles 
-#define MASK_TERRAIN (CATEGORY_BONES | CATEGORY_PROJECTILES | CATEGORY_INTERACTOR)  // Collides with bones and projectiles
-#define MASK_PROJECTILES  -1                                                        // Collides with everything
+#define MASK_BONES   (CATEGORY_TERRAIN | CATEGORY_PROJECTILES)                       // Collides with terrain and projectiles 
+#define MASK_TERRAIN (CATEGORY_BONES   | CATEGORY_PROJECTILES | CATEGORY_INTERACTOR) // Collides with bones and projectiles
+#define MASK_PROJECTILES  -1                                                         // Collides with everything
 #define MASK_PELLETS (CATEGORY_TERRAIN | CATEGORY_BONES)
-#define MASK_INTERACTOR (CATEGORY_TERRAIN)                                          // Collides with terrain only
+#define MASK_INTERACTOR CATEGORY_TERRAIN                                             // Collides with terrain only
 
 #define RAD2DEG(a) (((a) * 180.0) / M_PI)
 #define DEG2RAD(a) (((a) / 180.0) * M_PI)
