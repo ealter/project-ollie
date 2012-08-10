09489c75ffa927c6e7aa4f6f1877989eaf6d544c
Tucker Stone <tuckstone@gmail.com>
2012-08-09 16:41:39 -1000

    fucking git

--- ProjectOllie/GWObjects/GWWeaponTable.m
+++ ProjectOllie/GWObjects/GWWeaponTable.m
@@ -12,13 +12,7 @@
 
 @implementation GWWeaponTable
 
-<<<<<<< HEAD
--(id)init{
-    if (self = [super init]) {
-        [self scheduleUpdate];
-        self.isTouchEnabled = YES;
-    }
-=======
+
 +(id)viewWithDataSource:(id<SWTableViewDataSource>)dataSource size:(CGSize)size{
     
     GWWeaponTable* tv  = [self viewWithDataSource:dataSource size:size container:nil];
@@ -26,13 +20,11 @@
     tv.clipsToBounds = NO;
     [tv scheduleUpdate];
     return tv;
->>>>>>> fffff7237fc7b28cf33b0c3d15fe676df77b1b68
     
 }
 
 -(void)update:(ccTime) dt
 {
-<<<<<<< HEAD
     for (SWTableViewCell* cell in self->cellsUsed_) {
         
     }
@@ -40,11 +32,8 @@
 
 -(BOOL)ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    DebugLog(@"GALKDFJ");
-    return YES;
-=======
+    return NO;
 
->>>>>>> fffff7237fc7b28cf33b0c3d15fe676df77b1b68
 }
 
 - (void) registerWithTouchDispatcher {
